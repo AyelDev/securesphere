f7521a514b8ac5f2a665dd6b1b48be0402e9c7da
AyelDev <aarielabelgas23@gmail.com>
2024-12-22 08:35:10 +0800

    first commit

---
+++ src/main/java/com/ayeldev/securesphere/SecuresphereApplication.java
@@ -0,0 +1,13 @@
+package com.ayeldev.securesphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuresphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuresphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayeldev/securesphere/SecuresphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayeldev.securesphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuresphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
